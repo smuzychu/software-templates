08365d6838b0c87dfb09808a8c5babe27b11a47f
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2021-11-22 15:05:27 -0500

    added quarkus
    Signed-off-by: raffaelespazzoli <raffaele.spazzoli@gmail.com>

---
+++ scaffolder-templates/quarkus-template/skeleton/docs/index.md
@@ -0,0 +1,7 @@
+# ${{values.component_id | dump}}
+
+${{values.description | dump}}
+
+## Getting started
+
+Start by saying Hello, someone will respond saying Hi.
---
+++ scaffolder-templates/quarkus-template/skeleton/src/test/java/io/raffa/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.${{values.java_package_name}};
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
